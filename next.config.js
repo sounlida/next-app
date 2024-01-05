d86d172f492855b47d6354165c2eb2832f5d7f9b
lida62 <152981349+lida62@users.noreply.github.com>
2024-01-05 07:42:30 +0000

    Initial commit
    Created from https://vercel.com/new

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-  
-    images: {
-      unoptimized: true,
-    },
-    
-    reactStrictMode: true,
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {}
+
+module.exports = nextConfig
