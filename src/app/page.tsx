1b80dde5b78ed06c4969a6b2a7d16131c816f538
Ladi <42198382+sounlida@users.noreply.github.com>
2024-02-21 13:13:04 +0700

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,12 @@ export default function page() {
       <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed left-0 top-0 flex w-full flex-col-3 justify-center border-b  border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <a href="/" className="ml-3 mr-3 font-mono font-bold">
-            Home
+          <h6> Home </h6>
           </a>
           <a href="/shop" className="ml-3 mr-3 font-mono font-bold">
-            Shop
+          <h6> Shop </h6>
           </a>
-          <a href="/abouts" className="ml-3 mr-3 font-mono font-bold">Abouts Us</a>
+          <a href="/abouts" className="ml-3 mr-3 font-mono font-bold"><h6>Abouts Us </h6></a>
         </div>
 
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
