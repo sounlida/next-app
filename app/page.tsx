fdf21bcd5ac09c6e88abb013c866f75db29ef5e9
Sounlida <sounlida@gmail.com>
2024-03-01 01:39:51 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-import Search from './ui/search';
+
 import AcmeLogo from './ui/acme-logo';
 import ProductLists from './ui/products/product-lists';
 import Link from 'next/link';
@@ -14,7 +13,6 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <Search />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
