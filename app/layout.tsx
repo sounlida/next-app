ed0fdc0763736e60fda55c32f02ed4e0dd185eac
Sounlida <sounlida@gmail.com>
2024-03-01 03:13:00 +0700

    add
    Speed Insights
    Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { roboto } from '@/app/ui/fonts';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | ebuykh',
+    default: 'ebuykh',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  // metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 export default function RootLayout({
   children,
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased`}>{children}</body>
+      <body className={`${roboto.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   );
 }
