3c0ac310b486a966ab7d853c50c17fca667e2f5e
Sounlida <sounlida@gmail.com>
2024-03-03 02:49:31 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,11 @@
 'use client';
-import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { roboto } from '@/app/ui/fonts';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import { GoogleTagManager } from '@next/third-parties/google';
 
-export const metadata: Metadata = {
-  title: {
-    template: '%s | ebuykh',
-    default: 'ebuykh',
-  },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  // metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
-};
+
 export default function RootLayout({
   children,
 }: {
