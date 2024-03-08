4f9c06bf863f1bb6018c32c2889b08056cf2276d
Sounlida <sounlida@gmail.com>
2024-03-08 19:35:01 +0700

    add data

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/shop', title: 'Shop' },
+  { href: '/cart', title: 'Cart' },
+  { href: '/about', title: 'About' },
+];
+
+export default headerNavLinks;
