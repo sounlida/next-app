3a48b447ec490826736ada8b4c5c794588c03ca9
Sounlida <sounlida@gmail.com>
2024-03-09 09:45:08 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
+
       <body className={`${roboto.className} antialiased`}>
         {children}
         <SpeedInsights />
--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,10 @@ import Header from '@/components/Header';
 
 export default function Home() {
   return (
-    <main className='flex w-full min-h-screen flex-col items-center justify-between p-5'>
+    <main className='flex w-full min-h-screen flex-col items-center'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <Header />
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AcmeLogo />
-          </a>
-        </div>
+        <AcmeLogo />
       </div>
       <Lists />
       <div className="flex w-full items-center justify-between h-40 px-20 bg-blue-200">
