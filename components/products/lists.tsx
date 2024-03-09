e481271b63e4be8a95171a6163f2df3c4ffaccd0
Sounlida <sounlida@gmail.com>
2024-03-09 09:45:08 +0700

    update

--- components/products/lists.tsx
+++ components/products/lists.tsx
@@ -126,11 +126,11 @@ const products = [
 
 export default function Example() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <>
+      <div className="mx-auto mt-1 max-w-2xl px-4 py-2 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <div key={product.id} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
@@ -156,6 +156,6 @@ export default function Example() {
           ))}
         </div>
       </div>
-    </div>
+    </>
   )
 }
