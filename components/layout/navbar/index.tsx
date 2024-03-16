eb0c269ccdd80a63aa6ea771ba7bdd4922e1f920
Ladi <42198382+sounlida@users.noreply.github.com>
2024-03-16 14:26:47 +0700

    Update index.tsx

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,48 +1,63 @@
-
+'use client'
 
 import LogoSquare from '@/components/logo-square';
-import headerNavLinks from '@/data/headerNavLinks'
-import Link from 'next/link';
+import { usePathname } from 'next/navigation'
 import { Suspense } from 'react';
+import classnames from 'classnames'
+import Link from 'next/link';
+import Search from './search';
+import MobileNav from './MobileNav';
+import headerNavLinks from '@/lib/data/headerNavLinks'
 
 const { SITE_NAME } = process.env;
 
-export default async function Navbar() {
+const Navbar = () => {
+
+    const currentPath = usePathname();
 
 
-  return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6">
-      <div className="block flex-none md:hidden">
-        
-      </div>
-      <div className="flex w-full items-center">
-        <div className="flex w-full md:w-1/3">
-          <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
-            <LogoSquare />
-            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-              {SITE_NAME}
+    return (
+        <nav className="relative flex items-center justify-between p-4 lg:px-6">
+            <div className="block flex-none md:hidden">
+                <MobileNav />
             </div>
-          </Link>
-          <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-                {headerNavLinks
-                    .filter((link) => link.href !== '/')
-                    .map((link) => (
-                        <Link
-                            key={link.title}
-                            href={link.href}
-                            className="hidden h-6 text-2xl dark:text-gray-100 sm:block"
+            <div className="flex w-full items-center">
+                <div className="flex w-full md:w-1/3">
+                    <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
+                        <LogoSquare />
+                        <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
+                            {SITE_NAME}
+                        </div>
+                    </Link>
+                    <ul className="flex items-center leading-5 sm:space-x-6">
+                        <li
+                            className='hidden font-medium text-gray-900 dark:text-gray-100 space-x-4 sm:block'
                         >
-                            {link.title}
-                        </Link>
-                    ))}
+                            {headerNavLinks
+                                .map(link =>
+                                    <Link
+                                        key={link.href}
+                                        className={classnames({
+                                            'text-blue-900': link.href === currentPath,
+                                            'text-zinc-900': link.href !== currentPath,
+                                            'hover:text-blue-700 transition-color': true
+                                        })}
+                                        href={link.href}>{link.label}</Link>)}
+                        </li>
+                    </ul>
+
+                </div>
             </div>
-        <div className="hidden justify-center md:flex md:w-1/3">
-     
-        </div>
-        <div className="flex justify-end md:w-1/3">
-          <p>Cart</p>
-        </div>
-     
-    </nav>
-  );
+            <div className="hidden justify-center md:flex md:w-1/3">
+                <Search />
+            </div>
+            <div className="flex justify-end md:w-1/3">
+                <Suspense>
+                    <p>Cart</p>
+                </Suspense>
+            </div>
+        </nav>
+    )
 }
+
+export default Navbar
