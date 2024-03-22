2822cb41a2f92e95df972f6f05158f6a94cc498e
Sounlida <sounlida@gmail.com>
2024-03-22 08:52:01 +0700

    page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,12 @@ import { Suspense } from 'react';
 
 export const runtime = 'edge';
 
+export const metadata = {
+  description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+  openGraph: {
+    type: 'website'
+  }
+};
 export default async function Page() {
   return (
     <>
